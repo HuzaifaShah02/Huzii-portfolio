142846c7848506838c4bd850cf84d896e87ad71e
HuzaifaShah02 <156356348+HuzaifaShah02@users.noreply.github.com>
2024-03-17 15:49:33 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,15 +1,2 @@
-/* Base styles */
 
-/* Media queries for responsiveness */
-@media only screen and (max-width: 600px) {
-  /* Styles for screens less than or equal to 600px wide */
-}
-
-@media only screen and (min-width: 601px) and (max-width: 900px) {
-  /* Styles for screens between 601px and 900px wide */
-}
-
-@media only screen and (min-width: 901px) {
-  /* Styles for screens wider than 900px */
-}
 
